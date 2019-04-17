f3794420aa2aadaa0ea465d6ae7abdaa84db0e05
Amber Pittman <amber@amberpittman.com>
2019-04-17 19:58:14 -0400

    Working on Challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -62,12 +62,16 @@ console.log((interns[3].firstName));
 // Antonietta's Gender
 console.log((interns[4].gender));
 
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak("Hello, my name is Kennan!"));
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+function multiplyNums(a, b) {
+  let antonietta = a * b;
+}
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
