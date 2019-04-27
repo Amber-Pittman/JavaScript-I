c859b4b5a3b2d6a7af1126ae9c17b3d4f2e54983
Amber Pittman <amber@amberpittman.com>
2019-04-26 21:58:31 -0400

    Just saw a Need to Push notification

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -106,7 +106,9 @@ for (let i = 0; i < carYears.length; i++) {
 console.log(oldCars); 
 
 // ==== Challenge 6 ====
-// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only 
+// contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the 
+// results of the array in the console.
 
 let BMWAndAudi =[]; 
 
